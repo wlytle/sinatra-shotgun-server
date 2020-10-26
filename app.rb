1a102a022ebd3f4223faa535414b8ee20f8398f0
wlytle <wlytle89@gmail.com>
2020-10-25 17:43:05 -0700

    resolved shotgun errors

--- app.rb
+++ app.rb
@@ -1,9 +1,7 @@
-require_relative 'config/environment'
+require_relative "config/environment"
 
 class App < Sinatra::Base
-
-  get '/' do 
-    "Welcome to your app!!!! "
+  get "/" do
+    "Welcome to your app!!!! Cheese is good! fasdfa"
   end
-
 end
